d480a277442bed8e11b63b3d99836ca2f7a168e2
IamJoLo <insignificantuse@gmail.com>
2024-03-15 18:42:17 +0100

    first commit

---
+++ 1_Curiosity_Rover.py
@@ -0,0 +1,107 @@
+
+# Import statements
+import streamlit as st
+import pandas as pd
+from PIL import Image
+import requests
+from io import BytesIO
+
+# Set page config
+st.set_page_config(page_title="Mars Rover Images", page_icon=":computer:")
+
+
+
+# ### Teil 1:   *Überschrift:* Entdecke die Oberfläche des Mars  *Text:*  
+# Hallo, Weltraumforscher! Klick dich durch atemberaubende Aufnahmen des Mars und erfahre 
+# mehr über seine geheimnisvolle Oberfläche. Jedes Bild erzählt eine Geschichte aus einer fernen Welt. 
+# Entdecke Krater, Staubstürme und vielleicht sogar Hinweise auf Wasser! Mit jedem Bild, das du erkundest, 
+# kommst du dem Mars ein Stück näher. Bist du bereit, in die Fußstapfen eines echten Marsforschers zu treten? 
+
+
+st.title('MarsEntdecker')
+
+#image = open("bwl_ien.png", "rb").read()
+colx, coly = st.columns([2, 7])
+# Display the image in the first column
+with colx:
+    #st.image(image, caption='BWLien', use_column_width=True)
+    st.write("test")
+# Display text in the second column
+with coly:
+    st.write("""Willkommen, junger Astronaut! Ich bin das BWL-Lien, dein Guide auf dieser faszinierenden Reise durch das Weltall. 
+    Bist du bereit, die Geheimnisse des roten Planeten zu entdecken? Bei Mars Entdecker erwarten dich spannende Abenteuer und Spiele, die dein Wissen über den Mars erweitern. """)
+
+
+# Adding widgets to the main area
+st.title('Entdecke Mars-Aufnahmen')
+st.write("""Hallo, Weltraumforscher! Klick dich durch atemberaubende Aufnahmen des Mars Rovers Curiosity und erfahre 
+mehr über seine geheimnisvolle Oberfläche. Jedes Bild erzählt eine Geschichte aus einer fernen Welt. 
+Entdecke Krater, Staubstürme und vielleicht sogar Hinweise auf Wasser! Mit jedem Bild, das du erkundest, 
+kommst du dem Mars ein Stück näher. Bist du bereit, in die Fußstapfen eines echten Marsforschers zu treten?.
+
+Gib einfach ein Datum ein und clicke auf "Ergebnisse anzeigen"
+""")
+
+
+
+    
+# Input fields for year, month, and day
+col3, col2, col1 = st.columns(3)
+with col1:
+    year = st.number_input("Jahr", min_value=2012, max_value=2024, step=1, value = 2022)
+with col2:
+    month = st.number_input("Monat", min_value=1, max_value=12, step=1)
+with col3:
+    day = st.number_input("Tag", min_value=1, max_value=31, step=1)
+
+loop_interval = st.number_input("Anzahl ausgegebener Bilder?", min_value=1, step=1, value = 15)
+
+
+remaining_api_calls = 40
+# Function to display image from URL
+def display_image_from_url(url,description):
+    response = requests.get(url)
+    image = Image.open(BytesIO(response.content))
+    st.image(image, caption=description, use_column_width=True)
+
+if st.button("Ergebnisse anzeigen"):
+    # Call the NASA picture API
+    api_start = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date="
+    api_date = str(year)+"-"+str(month)+"-"+str(day)
+    api_date_rest = "&api_key="
+    api_key = "DEMO_KEY"
+    api_url = api_start + api_date + api_date_rest + api_key
+    print(api_url)
+    response = requests.get(api_url)
+    complete_json = response.json()
+    picture_urls = []
+    camera_description =[]
+    remaining_api_calls = response.headers["X-Ratelimit-Remaining"]
+    if len(complete_json["photos"]) == 0:
+        st.write("Für dieses Datum sind keine Daten verfügbar. Versuchen Sie einen anderen!")
+    for i in range(len(complete_json["photos"])):
+        picture_urls.append(complete_json["photos"][i]['img_src'])
+        camera_description.append(complete_json["photos"][i]["camera"]["full_name"])
+
+
+    # Streamlit UI
+    st.title('NASA Curiosity Mars Rover Bilder')
+
+    # Display images in two columns
+    col1, col2 = st.columns(2)
+    
+    
+    for i, url in enumerate(picture_urls):
+        with (col1 if i % 2 == 0 else col2):
+            description = "Curiosity Rover Bild: " + str(i+1) + " Kamera: " + camera_description[i]
+            if '.jpl' in url:
+                url = url.replace('.jpl', '')
+            display_image_from_url(url, description)
+            
+        if (i+1) % loop_interval ==0:
+            break
+
+    st.write("Du hast: " + str(remaining_api_calls) + " Aufrufe übrig (API calls).")
+
+
+
