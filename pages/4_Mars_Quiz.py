73d196d60aaa02db81565ef8b59881d414ae8651
IamJoLo <insignificantuse@gmail.com>
2024-04-25 18:56:57 +0200

    ASD

--- pages/4_Mars_Quiz.py
+++ pages/4_Mars_Quiz.py
@@ -40,17 +40,17 @@ st.divider()
 #Anzeige der Frage 2
 st.subheader("Frage 2: Wie hoch ist der höchste Berg auf dem Mars?")
 # Definition des Radiomenüs mit Antwortmeüs
-quiz_answer_1 = st.radio("Wähle die richtige Antwort aus", [4, 11, 25, 236])
+quiz_answer_2 = st.radio("Wähle die richtige Antwort aus", [4, 11, 25, 236])
 
 #Button um den Antwort auszugeben und Infotext auszugeben
 if st.button("Antwort überprüfen"):
-    infotext_1 = """Ein Marsjahr ist viel länger als ein Jahr auf der Erde! Der Mars benötigt 
-                    etwa 687 Tage, um die Sonne einmal zu umkreisen. Das liegt daran, dass seine 
-                    Umlaufbahn um die Sonne größer ist als die der Erde."""
-    if quiz_answer_1 == "687 Tage":
-        st.write(" Richtig gemacht! " + infotext_1)
+    infotext_2 = """Der Olympus Mons auf dem Mars ist der höchste Berg im ganzen Sonnensystem! Er ragt
+                    beeindruckende 25 Kilometer über die umliegende Ebene hinaus und ist damit 
+                    dreimal so hoch wie der Mount Everest auf der Erde."""
+    if quiz_answer_2 == 25:
+        st.write(" Richtig gemacht! " + infotext_2)
     else: 
-        st.write(" Fast. " + infotext_1)
+        st.write(" Fast. " + infotext_2)
 st.divider()
-#Der Olympus Mons auf dem Mars ist der höchste Berg im ganzen Sonnensystem! Er ragt beeindruckende 25 Kilometer über die umliegende Ebene hinaus und ist damit dreimal so hoch wie der Mount Everest auf der Erde.
+
 
