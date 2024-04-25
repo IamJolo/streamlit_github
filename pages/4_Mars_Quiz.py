d155692d6864547b98f4132cc2cba78a3aeecd4b
IamJoLo <insignificantuse@gmail.com>
2024-04-25 18:21:03 +0200

    asdf

--- pages/4_Mars_Quiz.py
+++ pages/4_Mars_Quiz.py
@@ -21,15 +21,18 @@ with coly:
     st.write("""Willkommen, junger Astronaut! In diesem spannenden Quiz kannst du dein Marswissen testen """)
 
 
-st.write("Frage 1: Wie viele Tage hat ein Jahr auf dem Mars?")
-quiz_answer_1 = st.radio("Pick one", ["365 Tage", "687 Tage", "55 Tage", "238 Tage"])
+st.subheader("Frage 1: Wie viele Tage hat ein Jahr auf dem Mars?")
+quiz_answer_1 = st.radio("Wähle die richtige Antwort aus", ["365 Tage", "687 Tage", "55 Tage", "238 Tage"])
 if st.button("Antwort überprüfen"):
+    infotext_1 = """Ein Marsjahr ist viel länger als ein Jahr auf der Erde! Der Mars benötigt 
+                    etwa 687 Tage, um die Sonne einmal zu umkreisen. Das liegt daran, dass seine 
+                    Umlaufbahn um die Sonne größer ist als die der Erde."""
     if quiz_answer_1 == "687 Tage":
-        st.write(""" Richtig gemacht! 
-                    Ein Marsjahr ist viel länger als ein Jahr auf der Erde! Der Mars benötigt 
-                    etwa 687 Tage, um die Sonne einmal zu umkreisen. Das liegt daran, dass seine 
-                    Umlaufbahn um die Sonne größer ist als die der Erde.""")
+        st.write(" Richtig gemacht! " + infotext_1)
     else: 
-        st.write(""" Fast, ein Marsjahr ist viel länger als ein Jahr auf der Erde! Der Mars benötigt 
-                    etwa 687 Tage, um die Sonne einmal zu umkreisen. Das liegt daran, dass seine 
-                    Umlaufbahn um die Sonne größer ist als die der Erde.""")
+        st.write(" Fast. " + infotext_1))
+
+st.divider()
+
+#Der Olympus Mons auf dem Mars ist der höchste Berg im ganzen Sonnensystem! Er ragt beeindruckende 25 Kilometer über die umliegende Ebene hinaus und ist damit dreimal so hoch wie der Mount Everest auf der Erde.
+
