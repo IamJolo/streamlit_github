9f593357af2220c98f18d20ccdb545cde3eafa94
IamJoLo <insignificantuse@gmail.com>
2024-05-05 02:14:19 +0200

    remove mathplotlib

--- pages/4_Mars_Quiz.py
+++ pages/4_Mars_Quiz.py
@@ -1,10 +1,8 @@
 # Import statements
 import streamlit as st
-import matplotlib as plt
+
 import pandas as pd
-from PIL import Image
-import requests
-from io import BytesIO
+
 
 # Seitenkonfiguration einstellen
 st.set_page_config(page_title="Mars Rover Images", page_icon=":computer:")
