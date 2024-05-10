7fc46751dd469d3482a538d0731c52072ef63b02
IamJoLo <insignificantuse@gmail.com>
2024-05-10 14:06:57 +0200

    button

--- pages/4_Mars_Quiz.py
+++ pages/4_Mars_Quiz.py
@@ -34,6 +34,7 @@ quiz_answer_1 = st.radio("Wähle die richtige Antwort aus", ["365 Tage", "687 Ta
 
 #Button um den Antwort auszugeben und Infotext auszugeben
 if st.session_state.check_answers:
+    infotext_displayed = True
     infotext_1 = """Ein Marsjahr ist viel länger als ein Jahr auf der Erde! Der Mars benötigt 
                     etwa 687 Tage, um die Sonne einmal zu umkreisen. Das liegt daran, dass seine 
                     Umlaufbahn um die Sonne größer ist als die der Erde."""
@@ -82,8 +83,7 @@ if st.session_state.check_answers:
     else:
         st.error("Leider falsch.")
         st.write(infotext_3)
-    st.divider()
-
+st.divider()
 # Frage 4
 st.subheader("Frage 4: Welches Raumschiff landete als erstes erfolgreich auf der Marsoberfläche?")
 quiz_answer_4 = st.radio("Wähle die richtige Antwort aus", ["Viking 1", "Mars Pathfinder", "Mars Science Laboratory (Curiosity)", "InSight"])
@@ -96,7 +96,7 @@ if st.session_state.check_answers:
     else:
         st.error("Falsch.")
         st.write(infotext_4)
-
+st.divider()
 # Frage 5
 st.subheader("Frage 5: Wie wird der Mars auch genannt?")
 quiz_answer_5 = st.text_input("Gib deine Antwort ein")
@@ -129,17 +129,18 @@ if st.session_state.check_answers:
     else:
         st.error("Falsch.")
         st.write(infotext_6)
-        
+
 # Antwort überprüfen
 if st.button("Antwort prüfen", key="quizbutton1"):
     st.session_state.check_answers = True
     
-    # Create DataFrame for bar chart
-    chart_data = pd.DataFrame({
-        "Antworten": ["Richtig", "Falsch"],
-        "Anzahl": [st.session_state.correct_answers_count, 4 - st.session_state.correct_answers_count],
-    })
+    if infotext_displayed: 
+        # Create DataFrame for bar chart
+        chart_data = pd.DataFrame({
+            "Antworten": ["Richtig", "Falsch"],
+            "Anzahl": [st.session_state.correct_answers_count, 6 - st.session_state.correct_answers_count],
+        })
 
-    # Display bar chart
-    st.subheader("Dies sind deine Resultate")
-    st.bar_chart(chart_data.set_index("Antworten"))
+        # Display bar chart
+        st.subheader("Dies sind deine Resultate")
+        st.bar_chart(chart_data.set_index("Antworten"))
